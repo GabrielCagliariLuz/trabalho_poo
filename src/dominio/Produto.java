46c0eddd21e0791f3c4a68974f211a4b942bd3c3
Gabriel Cagliari Luz <gabrielluz.git.hub@gmail.com>
2025-11-22 19:12:44 -0300

    Implementa enum

--- src/dominio/Produto.java
+++ src/dominio/Produto.java
@@ -4,11 +4,13 @@ public class Produto {
     private int codigo;
     private String nome;
     private double preco;
+    private TipoProduto tipo;
 
-    public Produto(int codigo, String nome, double preco) {
+    public Produto(int codigo, String nome, double preco, TipoProduto tipo) {
         this.codigo = codigo;
         this.nome = nome;
         this.preco = preco;
+        this.tipo = tipo;
     }
 
     public String getNome() {
@@ -21,6 +23,10 @@ public class Produto {
 
     public double getPreco() {
         return preco;
+    }
+
+    public TipoProduto getTipo() {
+        return tipo;
     }
 
     public void setPreco(double preco) {
---
+++ src/dominio/TipoProduto.java
@@ -0,0 +1,6 @@
+package dominio;
+
+public enum TipoProduto {
+    FISICO,
+    DIGITAL;
+}
